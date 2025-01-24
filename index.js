5a152ea4c846ae5c9cadb378a35770dcb6b290de
Damian Neal Hattingh <damiannhattingh@gmail.com>
2025-01-24 17:36:41 +0200

    Added Toggle menu with javascript required. Added responsiveness to website
    Added Toggle menu with javascript required. Added responsiveness to website

---
+++ index.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelector('.nav-links')
+
+function onToggleMenu(e) {
+    e.name = e.name === 'close' ? 'menu' : 'close'
+    navLinks.classList.toggle('top-[9%]')
+}
+
