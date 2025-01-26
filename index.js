c1041a1a9729155820593ac4bca6e00a212d83fc
Damian Neal Hattingh <damiannhattingh@gmail.com>
2025-01-26 21:24:56 +0200

    Added multiple pages to the website.
    Added multiple pages to the website.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function onToggleMenu(e) {
     navLinks.classList.toggle('top-[9%]')
 }
 
+
+//Nav bar links 
+
+function home() {
+    window.location.href = 'http://127.0.0.1:5500/home.html';
+}
+
+function shop() {
+    window.location.href = 'http://127.0.0.1:5500/shop.html';
+}
+
+function products() {
+    window.location.href = 'http://127.0.0.1:5500/products.html';
+}
+
+function story() {
+    window.location.href = 'http://127.0.0.1:5500/story.html';
+}
+
+function reviews() {
+    window.location.href = 'http://127.0.0.1:5500/reviews.html';
+}
+
+function packages() {
+    window.location.href = 'http://127.0.0.1:5500/packages.html';
+}
+
+function contact() {
+    window.location.href = 'http://127.0.0.1:5500/contact.html';
+}
+
